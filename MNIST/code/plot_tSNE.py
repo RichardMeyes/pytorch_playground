5aeb84c09e4ccdfecb68f3cc032354e1ad9b36d8
Richard Meyes <richard.meyes@ima.rwth-aachen.de>
2018-07-11 14:25:24 +0200

    added verbosity to tSNE plotting

--- MNIST/code/plot_tSNE.py
+++ MNIST/code/plot_tSNE.py
@@ -16,17 +16,25 @@ def plot_single_digits(trainloader):
         plt.show()
 
 
-def plot_tSNE(trainloader, num_samples):
+def plot_tSNE(testloader, num_samples):
     tsne = TSNE(n_components=2, perplexity=30, n_iter=5000, n_iter_without_progress=250,
-                init='pca', random_state=1337)
-    X_img = trainloader.dataset.train_data.numpy()[:num_samples]
-    Y = trainloader.dataset.train_labels.numpy()[:num_samples]
+                init='pca', random_state=None, verbose=4)
+    X_img = testloader.dataset.test_data.numpy()[:num_samples]
+    Y = testloader.dataset.test_labels.numpy()[:num_samples]
     X = X_img.reshape(-1, X_img.shape[1]*X_img.shape[2])  # flattening out squared images for tSNE
+
+    print("fitting tSNE...")
+    t0 = time.time()
     X_tsne = tsne.fit_transform(X)
+    t1 = time.time()
+    print("done! {0:.2f} seconds".format(t1 - t0))
 
     # scaling
     x_min, x_max = np.min(X_tsne, 0), np.max(X_tsne, 0)
     X_tsne = (X_tsne - x_min) / (x_max - x_min)
+
+    print("plotting tSNE...")
+    t0 = time.time()
 
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111)
@@ -39,8 +47,8 @@ def plot_tSNE(trainloader, num_samples):
     if hasattr(offsetbox, 'AnnotationBbox'):
         for i_digit in range(num_samples):
             # correct color for plotting
-            X_img[i_digit][X_img[i_digit, :, :] > 25] = 255
-            X_img[i_digit][X_img[i_digit, :, :] <= 25] = 0
+            X_img[i_digit][X_img[i_digit, :, :] > 10] = 255
+            X_img[i_digit][X_img[i_digit, :, :] <= 10] = 0
             imagebox = offsetbox.AnnotationBbox(offsetbox.OffsetImage(X_img[i_digit],
                                                                       cmap=custom_cmap,
                                                                       zoom=0.25),
@@ -51,6 +59,9 @@ def plot_tSNE(trainloader, num_samples):
     ax.set_title("KL: {}".format(tsne.kl_divergence_))
     plt.savefig("../plots/MNIST_tSNE.png")
     # plt.show()
+    t1 = time.time()
+    print("done! {0:.2f} seconds".format(t1 - t0))
+
 
 
 if __name__ == "__main__":
@@ -62,10 +73,7 @@ if __name__ == "__main__":
     testset = torchvision.datasets.MNIST(root='../data', train=False, download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=4)
     classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+    num_digits = testloader.dataset.test_labels.size()
 
     # plot_single_digits(trainloader)
-    print("plotting tSNE...")
-    t0 = time.time()
-    plot_tSNE(trainloader, num_samples=600)
-    t1 = time.time()
-    print("done! {0:.2f} seconds".format(t1 - t0))
+    plot_tSNE(testloader, num_samples=6000)
