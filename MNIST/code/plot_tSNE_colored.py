0f285dd119be091e12c5d4601d55882b2e4f9486
RichardMeyes <richard.meyes@ima.rwth-aachen.de>
2018-07-15 17:55:25 +0200

    refactoring and cleanup

--- MNIST/code/plot_tSNE_colored.py
+++ MNIST/code/plot_tSNE_colored.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 from matplotlib import offsetbox
 from matplotlib.colors import ListedColormap
 
+from sklearn.decomposition import PCA
+from MulticoreTSNE import MulticoreTSNE as TSNE
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -17,9 +20,33 @@ def plot_single_digits(trainloader):
         plt.show()
 
 
-def plot_tSNE(testloader, num_samples):
+def plot_tSNE(testloader, num_samples, fit=False, colored=True):
+    tsne = TSNE(n_components=2, perplexity=40, n_iter=10000, n_iter_without_progress=250,
+                init='random', random_state=None, verbose=4, n_jobs=12)
     X_img = testloader.dataset.test_data.numpy()[:num_samples]
     Y = testloader.dataset.test_labels.numpy()[:num_samples]
+
+    if fit:
+        X = X_img.reshape(-1, X_img.shape[1] * X_img.shape[2])  # flattening out squared images for tSNE
+
+        print("fitting PCA...")
+        t0 = time.time()
+        pca = PCA(n_components=30)
+        X = pca.fit_transform(X)
+        t1 = time.time()
+        print("done! {0:.2f} seconds".format(t1 - t0))
+
+        print("fitting tSNE...")
+        t0 = time.time()
+        X_tsne = tsne.fit_transform(X)
+        t1 = time.time()
+        print("done! {0:.2f} seconds".format(t1 - t0))
+
+        # scaling
+        x_min, x_max = np.min(X_tsne, 0), np.max(X_tsne, 0)
+        X_tsne = (X_tsne - x_min) / (x_max - x_min)
+
+        pickle.dump(X_tsne, open("../data/tSNE/X_tSNE_{0}.p".format(num_samples), "wb"))
 
     print("loading fitted tSNE coordinates...")
     X_tsne = pickle.load(open("../data/tSNE/X_tSNE_{0}.p".format(num_samples), "rb"))
@@ -28,6 +55,7 @@ def plot_tSNE(testloader, num_samples):
     t0 = time.time()
 
     fig = plt.figure(figsize=(10, 10))
+    fig.subplots_adjust(left=0.01, right=0.99, top=0.99, bottom=0.01)
     ax = fig.add_subplot(111)
 
     # define class colors
@@ -52,6 +80,12 @@ def plot_tSNE(testloader, num_samples):
             custom_cmap_colors[:, -1] = np.linspace(0, 1, custom_cmap.N)
             custom_cmap = ListedColormap(custom_cmap_colors)
 
+            if not colored:
+                custom_cmap = plt.cm.Greys
+                custom_cmap_colors = custom_cmap(np.arange(custom_cmap.N))
+                custom_cmap_colors[:, -1] = np.linspace(0, 1, custom_cmap.N)
+                custom_cmap = ListedColormap(custom_cmap_colors)
+
             # correct color for plotting
             X_img[i_digit][X_img[i_digit, :, :] > 10] = 255
             X_img[i_digit][X_img[i_digit, :, :] <= 10] = 0
@@ -62,7 +96,11 @@ def plot_tSNE(testloader, num_samples):
                                                 frameon=False,
                                                 pad=0)
             ax.add_artist(imagebox)
-    plt.savefig("../plots/MNIST_tSNE_{0}_colored.png".format(num_samples), dpi=1200)
+    ax.axis("off")
+    fig_path = "../plots/MNIST_tSNE_{0}_colored.png".format(num_samples)
+    if not colored:
+        fig_path = "../plots/MNIST_tSNE_{0}.png".format(num_samples)
+    plt.savefig(fig_path, dpi=1200)
     # plt.show()
     t1 = time.time()
     print("done! {0:.2f} seconds".format(t1 - t0))
@@ -80,4 +118,4 @@ if __name__ == "__main__":
     num_digits = testloader.dataset.test_labels.size()
 
     # plot_single_digits(trainloader)
-    plot_tSNE(testloader, num_samples=10000)
+    plot_tSNE(testloader, num_samples=10000, fit=False, colored=True)
