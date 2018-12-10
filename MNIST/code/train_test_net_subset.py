fixed wrong calculation of performance in the testing function and adapted it for subsampling the data

MNIST/code/train_test_net_subset.py
@@ -69,7 +69,6 @@ class Net(nn.Module):
         test_loss = 0
         correct = 0
         correct_class = np.zeros(10)
-        print(len(correct_class))
         correct_labels = np.array([], dtype=int)
         for i_batch, (data, target) in enumerate(testloader):
             data, target = Variable(data), Variable(target)
@@ -85,12 +84,12 @@ class Net(nn.Module):
                 label = target[i_label].item()
                 correct_class[label] += batch_labels[i_label].item()
             correct += batch_labels.sum()
-        test_loss /= len(testloader.dataset)
+        test_loss /= len(testloader.sampler)  # must consider the length of the sampler rather than of the whole dataset in case of subsampling!
         print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(test_loss, correct,
-                                                                                     len(testloader.dataset),
+                                                                                     len(testloader.sampler),
                                                                                      100. * correct.item() / len(
-                                                                                         testloader.dataset)))
-        acc = 100. * correct.item() / len(testloader.dataset)
+                                                                                         testloader.sampler)))
+        acc = 100. * correct.item() / len(testloader.sampler)
         # calculate class_acc
         acc_class = np.zeros(10)
         for i_label in range(10):
@@ -134,14 +133,13 @@ if __name__ == "__main__":
     testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=4)
     classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 
-    # Taking only part of the data
+    # Train with a
     mask0 = trainset.train_labels.numpy() == 0
     mask1 = trainset.train_labels.numpy() == 1
     mask_train = mask0 | mask1
     mask0 = testset.test_labels.numpy() == 0
     mask1 = testset.test_labels.numpy() == 1
     mask_test = mask0 | mask1
-    print(len(mask_test), mask_test.sum())
     my_sampler_train = torch.utils.data.SubsetRandomSampler(np.squeeze(np.argwhere(mask_train)))
     my_sampler_test = torch.utils.data.SubsetRandomSampler(np.squeeze(np.argwhere(mask_test)))
     trainloader_sub01 = torch.utils.data.DataLoader(trainset, batch_size=256, sampler=my_sampler_train,
@@ -153,7 +151,7 @@ if __name__ == "__main__":
         plot_data()
 
     if train:
-        net.train_net(criterion, optimizer, trainloader_sub01, epochs=1, device=device)
+        net.train_net(criterion, optimizer, trainloader_sub01, epochs=5, device=device)
     else:
         net.load_state_dict(torch.load('../nets/MNIST_MLP(20, 10)_trained.pt'))
         net.eval()